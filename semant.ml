2cce1629fba61b41f907e7ec1a82f95c67c9acca
Victor <victor@canelatools.com>
2014-03-07 17:45:38 +0100

    check vardec

--- semant.ml
+++ semant.ml
@@ -21,15 +21,46 @@ and check_decl venv tenv decl =
      check_vardec venv tenv name typ init pos
 
 and check_fundec venv tenv name ret_typ args body pos =
-  let arg_types = List.map (fun (_, arg_typ) -> actual_type tenv arg_typ pos) args in
+  assert_dup venv name pos;
+  let arg_types = List.map
+                    (fun (_, arg_typ) -> actual_type tenv arg_typ pos)
+                    args in
   let typ = actual_type tenv ret_typ pos in
-  let fun_entry = E.FunEntry (arg_types, typ) in
-  let venv' = S.put venv name fun_entry in
+  let entry = E.FunEntry (arg_types, typ) in
+  let venv' = S.put venv name entry in
   (venv', tenv, T.Unit)
 
 and check_vardec venv tenv name typ init pos =
-  (* TODO *)
-  (venv, tenv, T.Unit)
+  assert_dup venv name pos;
+  let typ = actual_type tenv typ pos in
+  begin
+    match init with
+    | None -> ()
+    | Some(init) ->
+       begin
+         let exp_type = check_exp venv tenv init pos in
+         assert_type typ exp_type pos;
+       end
+  end;
+  let entry = E.VarEntry typ in
+  let venv' = S.put venv name entry in
+  (venv', tenv, T.Unit)
+
+and check_exp venv tenv exp pos =
+  match exp with
+  | A.NilExp (_) ->
+     T.Nil
+  | A.IntExp (_,_) ->
+     T.Int
+  | A.VarExp (_,_) ->
+     (* TODO *)
+     T.Int
+  | A.CallExp (_,_,_) ->
+     (* TODO *)
+     T.Int
+  | A.OpExp (_,_,_,_) ->
+     (* TODO *)
+     T.Int
 
 and actual_type tenv sym pos =
   match S.get tenv sym with
@@ -47,8 +78,12 @@ and error msg pos =
   and col = Lexer.col pos in
   Printf.printf "%d:%d: %s" line col msg
 
-(*
 and assert_type t1 t2 pos =
   if t1<>t2 then
     error "types does not match" pos
-*)
+
+and assert_dup env sym pos =
+  match S.get env sym with
+  | None -> ()
+  | Some _ ->
+     error ("Already defined "^(S.name sym)) pos
