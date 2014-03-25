e39889da4bee5536ac66f13df7b2047b89169959
Victor M <samsaga2@gmail.com>
2014-03-25 12:38:44 +0100

    catch parser errors

--- minicsharp.ml
+++ minicsharp.ml
@@ -1,5 +1,12 @@
 let _  =
-  let lexbuf = Lexing.from_channel stdin in
-  let prog = Parser.program Lexer.token lexbuf in
-  let ir = Semant.check prog in
-  ignore ir
+  let in_buffer = stdin in
+  let lexbuf = Lexing.from_channel in_buffer in
+  try
+    let prog = Parser.program Lexer.token lexbuf in
+    ignore (Semant.check prog)
+  with
+  | Parser.Error -> 
+     let pos = Lexing.lexeme_start_p lexbuf in
+     let lnum = pos.Lexing.pos_lnum
+     and cnum = pos.Lexing.pos_cnum - pos.Lexing.pos_bol + 1 in
+     Printf.fprintf stderr "%d:%d: syntax error\n%!" cnum lnum
